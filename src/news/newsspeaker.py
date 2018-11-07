0f06e4fae4b37bd3e59ab3f798d411385503c675
Pascal <pscl.lbssr@gmail.com>
2018-11-07 13:47:36 +0100

    add newsspeaker module to generate wav file from given input string

---
+++ src/news/newsspeaker.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+# HTTP + URL packages
+import httplib2
+import urllib # For URL creation
+
+# Mary server informations
+tts_host = "localhost"
+tts_port = "59125"
+
+tts_audio_location = "/home/pi/radio/rendered/news/"
+
+
+def readNews(text):
+
+    # Build the query
+    query_hash = {"INPUT_TEXT":text,
+                  "INPUT_TYPE":"TEXT", # Input text
+                  "LOCALE":"en_US",
+                  "VOICE":"cmu-slt-hsmm", # Voice informations  (need to be compatible)
+                  "OUTPUT_TYPE":"AUDIO",
+                  "AUDIO":"WAVE", # Audio informations (need both)
+                  }
+    query = urllib.urlencode(query_hash)
+    print("query = \"http://%s:%s/process?%s\"" % (tts_host, tts_port, query))
+
+    # Run the query to mary http server
+    h_mary = httplib2.Http()
+    resp, content = h_mary.request("http://%s:%s/process?" % (tts_host, tts_port), "POST", query)
+
+    #  Decode the wav file or raise an exception if no wav files
+    if (resp["content-type"] == "audio/x-wav"):
+
+        # Write the wav file
+        f = open(tts_audio_location + "/news.wav", "wb")
+        f.write(content)
+        f.close()
+
+    else:
+        raise Exception(content)
---
+++ src/radio.py
@@ -0,0 +1,7 @@
+# Example to consume MaryTTS: https://github.com/marytts/marytts-txt2wav/blob/python/txt2wav.py
+from news import newsspeaker
+
+# Input text
+input_text = "Hello Alex you are great!"
+
+newsspeaker.readNews(input_text)
