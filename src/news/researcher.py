fa962273c89c54fdc35f30815f6597c7c5bf7138
Pascal <pscl.lbssr@gmail.com>
2019-01-04 11:58:18 +0100

    add cronjob entry point, python libraries

--- src/news/researcher.py
+++ src/news/researcher.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import news_config
+import datetime
+import pytz
 
 def getWeather():
     import pyowm
@@ -13,22 +15,50 @@ def getWeather():
     weather_berne = observation_berne.get_weather().get_temperature('celsius')
     weather_zurich = observation_zurich.get_weather().get_temperature('celsius')
     weather_chur = observation_chur.get_weather().get_temperature('celsius')
-    
-    weather_text = news_config.WEATHER_TEXT.format(
-        weather_berne['temp_min'], 
-        weather_berne['temp_max'], 
-        weather_chur['temp_min'], 
-        weather_chur['temp_max'], 
-        weather_zurich['temp_min'],
-        weather_zurich['temp_max'])     
-
 
     # forecast to use later
-    forecaster = owm.three_hours_forecast('Bern,CH')
-    forecast = forecaster.get_forecast()
-    for weather in forecast:
-        w = weather
-        #print (weather.get_reference_time('iso') + ": " + str(weather.get_temperature('celsius')['temp']))
+    forecaster_berne = owm.three_hours_forecast('Bern,CH')
+    forecast_berne = forecaster_berne.get_forecast()
+    
+    forecaster_chur = owm.three_hours_forecast('Chur,CH')
+    forecast_chur = forecaster_chur.get_forecast()
+    
+    forecaster_zurich = owm.three_hours_forecast('Zurich,CH')
+    forecast_zurich = forecaster_zurich.get_forecast()
+   
+    # desired forecast datetime (tomorrow 12:00)   
+    utc = pytz.UTC
+    desired_date = datetime.datetime.combine(datetime.date.today(), datetime.time(hour=12)) + datetime.timedelta(days=1)
+    desired_date= utc.localize(desired_date)
+    
+    for weather in forecast_berne:
+        if weather.get_reference_time('date') == desired_date:
+            berne_forecast_status = weather.get_status()
+            berne_forecast_temp = weather.get_temperature(unit='celsius')['temp']
+            
+    for weather in forecast_chur:
+        if weather.get_reference_time('date') == desired_date:
+            chur_forecast_status = weather.get_status()
+            chur_forecast_temp = weather.get_temperature(unit='celsius')['temp']
+            
+    for weather in forecast_zurich:
+        if weather.get_reference_time('date') == desired_date:
+            zurich_forecast_status = weather.get_status()
+            zurich_forecast_temp = weather.get_temperature(unit='celsius')['temp']
+            
+    weather_text = news_config.WEATHER_TEXT.format(
+        int(weather_berne['temp_min']), 
+        int(weather_berne['temp_max']), 
+        int(weather_chur['temp_min']), 
+        int(weather_chur['temp_max']), 
+        int(weather_zurich['temp_min']),
+        int(weather_zurich['temp_max']),
+        berne_forecast_status,
+        int(berne_forecast_temp),
+        chur_forecast_status,
+        int(chur_forecast_temp),
+        zurich_forecast_status,
+        int(zurich_forecast_temp)) 
     
     #print(weather_text)
     return weather_text
@@ -41,7 +71,8 @@ def getNews():
     
     news = ''
     hour = time.localtime().tm_hour + 1
-    news += "Es ist " + str(hour) +" Uhr, willkommen zu den News auf Sparprogramm.fm - Sparprogramm statt Radioprogramm "
+    #news += "Es ist " + str(hour) +" Uhr, willkommen zu den News auf Sparprogramm.fm"
+    news += "Du hörst Sparprogramm.fm und es ist Zeit für die Nachrichten."
 
     resp = requests.get('https://api.presseportal.ch/api/article/all?&format=json&lang=de&teaser=1&limit=3&api_key=' + news_config.PRESSEPORTAL_API_KEY)
     if resp.status_code != 200:
@@ -63,7 +94,7 @@ def _getNewsIntro():
     
     news = ''
     hour = time.localtime().tm_hour + 1
-    news += "Es ist " + str(hour) +" Uhr, willkommen zu den News auf Sparprogramm.fm - Sparprogramm statt Radioprogramm \n"
+    news += "Es ist " + str(hour) +" Uhr, willkommen zu den News auf Sparprogramm.fm \n"
     return news
 
 def getAlternativeFacts():
@@ -87,6 +118,8 @@ def getNewsMix():
     news_text = news_text + scraper.scrapeTXT(news_config.NEWS_SOURCE_1, 2)
     news_text = news_text + scraper.scrapeTXT(news_config.NEWS_SOURCE_2, 1)
     news_text = news_text + scraper.scrapeTXT(news_config.NEWS_SOURCE_3, 1)
+    
+    news_text = news_text + news_config.NEWS_OUTRO
     
     return news_text
 
