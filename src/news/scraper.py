1160e73bf8e80c794d8f8831c5b84f7ca1d21019
Pascal <pscl.lbssr@gmail.com>
2019-01-04 16:05:38 +0100

    remove problems with utf-8 encodings

--- src/news/scraper.py
+++ src/news/scraper.py
@@ -5,14 +5,14 @@ def scrapeTXT(url, limit):
     
     newsTxt = ''
     
-    page = requests.get(url)
+    page = requests.get(url)    
     soup = BeautifulSoup(page.content, 'html.parser')
 
     articles = soup.find_all('article')
 
     for article in articles[0:limit]:
-        title = article.find('h2').get_text()
-        content = article.find('div', class_="inner").find('p').get_text()
+        title = article.find('h2').get_text().encode("utf-8","ignore")
+        content = article.find('div', class_="inner").find('p').get_text().encode("utf-8","ignore")
         
         newsTxt = newsTxt + title + content
         
