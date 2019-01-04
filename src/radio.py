2b1b61139f91c27bf1ff0020f66d69c5e8c11704
Pascal <pscl.lbssr@gmail.com>
2019-01-04 19:12:17 +0100

    add boulevard to spice it up with some cheesy chees

--- src/radio.py
+++ src/radio.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 from news import newsspeaker, researcher
 from music import music
+from boulevard import diecheese
 import radio_config
 import facility_manager
 
@@ -8,18 +8,25 @@ import sys
 import time
 
 time_string = time.strftime('%Y%m%d_%H')
-log_file = "/home/pi/radio/rendered/logs/" + time_string + "_log.txt"
+log_file = "/home/pi/radio/logs/" + time_string + "_log.txt"
 sys.stdout = open(log_file, "w")
 
+# make weather forecast
 weather_text = researcher.getWeather()
-print('Weather: ' + weather_text)
+#print('Weather: ' + weather_text)
 newsspeaker.readWithGoogle(weather_text, "weather_forecast")
 
+# produce news
 news_text = researcher.getNewsMix()
 #print('News:' + news_text)
 newsspeaker.readWithGoogle(news_text, "news")
 
-#music.fetchMusic()
+# get music files from server
+music.fetchMusic()
+
+# Most popular diecheese article
+article_text = diecheese.getMostPopularArticle()
+newsspeaker.readWithGoogle(article_text, "most_popular_cheese")
 
 sys.stdout.close()
 
